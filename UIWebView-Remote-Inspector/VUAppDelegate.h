62a40fb7f7fb90ed5f44706e0e52dddc92d50a57
NeoNacho <boris@icculus.org>
2011-12-04 12:21:54 +0100

    Created Xcode project.

---
+++ UIWebView-Remote-Inspector/VUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VUAppDelegate.h
+//  UIWebView-Remote-Inspector
+//
+//  Created by Boris Bügling on 04.12.11.
+//  Copyright (c) 2011 Extessy AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIWebView-Remote-Inspector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIWebView-Remote-Inspector
+//
+//  Created by Boris Bügling on 04.12.11.
+//  Copyright (c) 2011 Extessy AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VUAppDelegate class]));
+    }
+}
